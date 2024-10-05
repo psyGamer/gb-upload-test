62c66a0afa55906b798872dcf729bbc5f9be7e6b
psyGamer <psygamer21@gmail.com>
2024-10-05 20:28:16 +0200

    Update gamebanana-upload.py

--- scripts/gamebanana-upload.py
+++ scripts/gamebanana-upload.py
@@ -26,8 +26,8 @@ def main():
 
     # Authenticate
     twofac_code = compute_twofac_code(os.getenv("GAMEBANANA_2FA_URI"))
-    fetch_result = driver.execute_script(fr"""
-        return fetch('https://gamebanana.com/apiv11/Member/Authenticate', {{
+    driver.execute_script(fr"""
+        fetch('https://gamebanana.com/apiv11/Member/Authenticate', {{
             'headers': {{
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
                 'Accept': 'application/json, text/plain, */*',
@@ -49,7 +49,7 @@ def main():
             'credentials': 'include'
     }});
     """.replace('\n', ''))
-    print(fetch_result)
+    print(driver.execute("return document.body.outerHTML"))
     driver.implicitly_wait(3)
     time.sleep(3)
 
